76ecd99588ff877fdfb67fa4d6140b4d9069dd09
Joko S <sudarsonojoko8@gmail.com>
2019-08-24 23:55:22 +0700

    app.js update

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const Operation = require('./utils/Operation')
 
 const calculator = new Calculator()
 
+console.log('sum >>> ', calculator.compute(Operation.sum, 1, 2))
+console.log('sum >>> ', calculator.compute(Operation.multiply, 1, 2))
 console.log('sum >>> ', calculator.compute(Operation.prime, 4))
+console.log('sum >>> ', calculator.compute(Operation.fibonacci, 4))
